5ad75617cfc5e308c1f9fce61f27f8de184fad10
tiger <tiger@unallocated.barefruit.co.uk>
2020-11-06 18:49:16 -0800

    Worked on FCFS Implementation Added Event Class

--- ProcessScheduling/src/Main.java
+++ ProcessScheduling/src/Main.java
@@ -1,8 +1,80 @@
+import java.util.List;
 
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is our CPU Scheduling project");
+		//System.out.println("This is our CPU Scheduling project");
+		 long id = getPID();
+		 int pid = (int) id;
+        System.out.println("-----------------Running First Come First Serve (FCFS)----------------");
+        fcfs(pid);
+        System.out.println("-----------------Running Shortest Job First (SJF)-----------------");
+        sjf(pid);
+        System.out.println("-----------------Running Priority-----------------");
+        ps(pid);
+        System.out.println("-----------------Running RoundRobin(RR)------------------");
+        rr(pid);
 	}
+        
+        public static void fcfs(int pid)
+        {
+        	Scheduler fcfs = new FirstComeFirstServe();
+        	fcfs.add(new Process(pid, "P1", 0, 5));
+        	fcfs.add(new Process(pid, "P2", 2, 4));
+        	fcfs.add(new Process(pid, "P3", 4, 3));
+        	fcfs.add(new Process(pid, "P4", 6, 6));
+        	fcfs.process();
+        	display(fcfs);
+        }
+        
+        public static void sjf(int pid)
+        {
+        }
+        
+        public static void ps(int pid)
+        {
+        }
+        
+        public static void rr(int pid)
+        {
+        }
+        
+        public static void display(Scheduler obj)
+        {
+            System.out.println("pid\tProcess\tAT\tBT\tWT\tTAT");
+
+            for (Process prc : obj.getProcess())
+            {
+                System.out.println(prc.getPID() + "\t" +  prc.getProcessName() + "\t" + prc.getArrivalTime() + "\t" + prc.getBurstTime() + "\t" + prc.getWaitingTime() + "\t" + prc.getTurnaroundTime());
+            }
+            
+            System.out.println();
+            
+            for (int i = 0; i < obj.getTimeline().size(); i++)
+            {
+                List<Event> prc_timeline = obj.getTimeline();
+                System.out.print(prc_timeline.get(i).getStartTime() + "(" + prc_timeline.get(i).getProcessName() + ")");
+                
+                if (i == obj.getTimeline().size() - 1)
+                {
+                    System.out.print(prc_timeline.get(i).getFinishTime());
+                }
+            }
+            
+            System.out.println("\n\nAverage Wait Time: " + obj.getAverageWaitingTime() + "\nAverage Turn Arounf Time: " + obj.getAverageTurnAroundTime());
+        }
+        
+        public static long getPID() {
+            String processName = java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
+            if (processName != null && processName.length() > 0) {
+                try {
+                    return Long.parseLong(processName.split("@")[0]);
+                }
+                catch (Exception e) {
+                    return 0;
+                }
+            }
+
+            return 0;
+        }
 }
--- ProcessScheduling/src/Scheduler.java
+++ ProcessScheduling/src/Scheduler.java
@@ -1,4 +1,94 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class Scheduler {
-
+public abstract class Scheduler {
+	private final List<Process> prcs;
+	 private final List<Event> prc_timeline;
+    private int timeQuantum;
+    
+    public Scheduler()
+    {
+    	prcs = new ArrayList<Process>();
+    	prc_timeline = new ArrayList<Event>();
+        timeQuantum = 1;
+    }
+    
+    public boolean add(Process prc)
+    {
+        return prcs.add(prc);
+    }
+    
+    public void setTimeQuantum(int timeQuantum)
+    {
+        this.timeQuantum = timeQuantum;
+    }
+    
+    public int getTimeQuantum()
+    {
+        return timeQuantum;
+    }
+    
+    public double getAverageWaitingTime()
+    {
+        double avg = 0.0;
+        
+        for (Process prc : prcs)
+        {
+            avg += prc.getWaitingTime();
+        }
+        
+        return avg / prcs.size();
+    }
+    
+    public double getAverageTurnAroundTime()
+    {
+        double avg = 0.0;
+        
+        for (Process prc : prcs)
+        {
+            avg += prc.getTurnaroundTime();
+        }
+        
+        return avg / prcs.size();
+    }
+    
+    public Event getEvent(Process prc)
+    {
+        for (Event event : prc_timeline)
+        {
+            if (prc.getProcessName().equals(event.getProcessName()))
+            {
+                return event;
+            }
+        }
+        
+        return null;
+    }
+    
+    public Process getProcess(String process)
+    {
+        for (Process prc : prcs)
+        {
+        	 if (prc.getProcessName().equals(process))
+            {
+                return prc;
+            }
+        }
+        
+        return null;
+    }
+    
+    public List<Process> getProcess()
+    {
+        return prcs;
+    }
+    
+    public List<Event> getTimeline()
+    {
+        return prc_timeline;
+    }
+    
+    
+    
+    public abstract void process();
 }
